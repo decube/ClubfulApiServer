ec27a388782511e6a605881331a8bd1780831f9e
smkim89 <kiak357@gmail.com>
2016-09-03 15:41:18 +0900

    index 불필요한거 제거

--- index.js
+++ index.js
@@ -7,30 +7,14 @@ var router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
-    res.json({ code : true ,message: 'Added' });
-});
-
-app.post('hi', function(req, res){
-  var title = req.body.title;
-  res.send(title);
-});
-
-app.route('/book')
-  .get(function(req, res) {
-    res.send('Get a random book');
-  })
-  .post(function(req, res) {
-    res.send('Add a book');
-  })
-  .put(function(req, res) {
-    res.send('Update the book');
-  });
 
 var versionCheck = require('./src/router/versionCheck.js');
 var join = require('./src/router/join.js');
+var join = require('./src/router/court.js');
+
 app.use('/versionCheck', versionCheck);
 app.use('/join', join);
+app.use('/court', court);
 
 app.listen(3000, function(){
     console.log('Connection 3000 port!');
