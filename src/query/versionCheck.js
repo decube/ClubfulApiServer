4129305c09d13eea2c504e50f04f3fa6b8d7ebc2
smkim89 <kiak357@gmail.com>
2016-09-10 15:09:29 +0900

    table 설계 수정(device,user)

--- src/query/versionCheck.js
+++ src/query/versionCheck.js
@@ -5,7 +5,7 @@ module.exports = function() {
           getNewNoticeVer: 'SELECT notice_ver as ver FROM NOTICE_VER ORDER BY MAKE_DT DESC LIMIT 1',
           getNewDeviceVer: 'SELECT ver FROM DEVICE_VER WHERE DEVICE =? AND LANGUAGE=? ORDER BY MAKE_DT DESC LIMIT 1',
           getCategoryList: 'select seq, category_nm from category where ver = ? order by seq asc',
-          insertDevice : 'INSERT INTO DEVICE (token, device_type, version, language, uuid, make_dt, update_dt) values(?,?,?,?,?,now(),now())',
+          insertDevice : 'INSERT INTO DEVICE (token, device_type, version, language, uuid, make_dt, update_dt, device_ver_seq) values(?,?,?,?,?,now(),now(),(select seq from device_ver order by seq desc limit 1))',
           getTokenSeq : "SELECT fn_get_seq('token')",
       };
   }
