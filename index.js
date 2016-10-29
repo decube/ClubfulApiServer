9f58be8adafcd4abfa2055547e928e7d184b5bf4
smkim89 <kiak357@gmail.com>
2016-10-29 17:16:23 +0900

    서버 적용 db query update

--- index.js
+++ index.js
@@ -13,31 +13,11 @@ var court = require('./src/router/court.js');
 var user = require('./src/router/user.js');
 var appjs = require('./src/router/app.js');
 
-app.use('/version', versionCheck, {
-    setHeaders: function(res) {
-        res.setHeader("Content-Type", "application/json");
-    }
-});
-app.use('/certification', certification, {
-    setHeaders: function(res) {
-        res.setHeader("Content-Type", "application/json");
-    }
-});
-app.use('/court', court, {
-    setHeaders: function(res) {
-        res.setHeader("Content-Type", "application/json");
-    }
-});
-app.use('/user', user, {
-    setHeaders: function(res) {
-        res.setHeader("Content-Type", "application/json");
-    }
-});
-app.use('/app', appjs, {
-    setHeaders: function(res) {
-        res.setHeader("Content-Type", "application/json");
-    }
-});
+app.use('/version', versionCheck);
+app.use('/certification', certification);
+app.use('/court', court);
+app.use('/user', user);
+app.use('/app', appjs);
 
 app.listen(8080, function(){
     console.log('Connection 8080 port!');
