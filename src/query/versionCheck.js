9c6e07a73b37f5187abd3434a6f30e7b39305c3a
smkim89 <kiak357@gmail.com>
2016-10-29 17:20:51 +0900

    서버 적용 db query update

--- src/query/versionCheck.js
+++ src/query/versionCheck.js
@@ -5,7 +5,7 @@ module.exports = function() {
           getNewNoticeVer: 'SELECT noticeVer as ver FROM notice_ver ORDER BY makeDT DESC LIMIT 1',
           getNewDeviceVer: 'SELECT ver FROM device_ver WHERE device =? AND language=? ORDER BY makeDT DESC LIMIT 1',
           getCategoryList: 'select seq, categoryNM from category where ver = ? order by seq asc',
-          insertDevice : 'INSERT INTO device (token, deviceType, version, language, uuid, makeDT, updateDT, deviceVerSeq) values(?,?,?,?,?,now(),now(),(select seq from device_ver order by seq desc limit 1))',
+          insertDevice : 'INSERT INTO device (token, deviceType, language, uuid, makeDT, updateDT, deviceVerSeq) values(?,?,?,?,now(),now(),(select seq from device_ver order by seq desc limit 1))',
           getTokenSeq : "SELECT fnGetSeq('token')",
       };
   }
