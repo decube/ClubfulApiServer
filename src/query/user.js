14ed1579c0c121e9e5bdefcdaa9d02964a917b34
smkim89 <kiak357@gmail.com>
2016-12-04 17:39:20 +0900

    .

--- src/query/user.js
+++ src/query/user.js
@@ -3,7 +3,7 @@ module.exports = function() {
           insertUser : 'INSERT INTO user_info (userID, password, nickName, sex, birthDay, userLatitude, userLongitude, userAddress, userAddressShort, isNoticePush, isInsertPush, isDistancePush, isInterestPush, pushStartTime, pushEndTime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
           checkUser : 'SELECT * FROM user_info WHERE userID = ?',
           updateDevice : 'update device set gcmID = ? , userID =? where token = ?',
-          getUserDevice : 'select * from user_info u, device d where u.userID = d.userID and u.userID = ?',
+          getUserDevice : 'select u.* from user_info u, device d where u.userID = d.userID and u.userID = ?',
           updateUser : 'update user_info set password=?, nickName=?, sex=?, birthDay=?,userLatitude=?,userLongitude=?, userAddress=?, userAddressShort=? where userID = ?',
           logoutCheck : 'select * from user_info u, device d where u.userID = d.userID and u.userID = ? and d.token = ?',
           updateUserSetting : 'update user_info set isNoticePush=?, isInsertPush=?, isInterestPush=?, isDistancePush=?,pushStartTime=?,pushEndTime=? where userID = ?',
