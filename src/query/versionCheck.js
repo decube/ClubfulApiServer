abec38d1a9c27c362defb5e7bab53c8fa593d810
smkim89 <kiak357@gmail.com>
2016-12-31 16:28:29 +0900

    init

--- src/query/versionCheck.js
+++ src/query/versionCheck.js
@@ -4,7 +4,7 @@ module.exports = function() {
           getNewCategoryVer: 'SELECT ver FROM category_ver ORDER BY makeDT DESC LIMIT 1',
           getNewNoticeVer: 'SELECT noticeVer as ver FROM notice_ver ORDER BY makeDT DESC LIMIT 1',
           getNewDeviceVer: 'SELECT ver FROM device_ver WHERE device =? AND language=? ORDER BY makeDT DESC LIMIT 1',
-          getCategoryList: 'select seq, categoryNM from category where ver = ? order by seq asc',
+          getCategoryList: "select seq, categoryNM from category where ver = ? and status='Y' order by seq asc",
           insertDevice : 'INSERT INTO device (token, deviceType, language, uuid, makeDT, updateDT, deviceVerSeq) values(?,?,?,?,now(),now(),(select seq from device_ver where device = ? order by seq desc limit 1))',
           getTokenSeq : "SELECT fnGetSeq('token') as tokenSeq",
           getNewestDevice : 'select * from device_ver where device=? ORDER BY seq DESC LIMIT 1'
